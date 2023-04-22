1a2d1e9356f7e8be932daea5d99ff150ac4a380d
Hamanpreet <haman42626@gmail.com>
2023-04-22 16:06:19 -0400

    Reverse CLI

--- reverseString.js
+++ reverseString.js
@@ -1,19 +1,22 @@
 const reverse = function() {
   //let arr = Array.from(str);
   //console.log(arr);
- let result= [];
- let revStr = "";
+ 
+ 
 for(let y = 2; y < process.argv.length; y++) {
+  let revStr = "";
+  let result= [];
  for(let i = (process.argv[y].length - 1); i >= 0;i--) {
   result.push(process.argv[y][i]);
  }
-}
- 
-for( let i = 0; i < result.length; i++) {
+ for( let i = 0; i < result.length; i++) {
   revStr += result[i];
  }
- return revStr;
+ console.log(revStr);
+}
 };
+ 
 
-console.log(reverse());
+//const myInput = "hello goodbye";
+reverse();
 
